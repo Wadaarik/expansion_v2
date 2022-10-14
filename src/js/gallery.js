6a7f31c746fe4a803b9cd2a23f801d4850b7de1f
Wadaarik <nicolasboudier12@gmail.com>
2022-10-15 00:00:40 +0200

    first commit

---
+++ src/js/gallery.js
@@ -0,0 +1,20 @@
+import MiniMasonry from "minimasonry";
+
+var masonry2 = new MiniMasonry({
+    container: document.querySelector('.container_2'),
+    gutterX: 40,
+    gutterY: 40,
+    baseWidth: 425
+});
+var masonry3 = new MiniMasonry({
+    container: document.querySelector('.container_3'),
+    gutterX: 40,
+    gutterY: 40,
+    baseWidth: 425
+});
+var masonry4 = new MiniMasonry({
+    container: document.querySelector('.container_4'),
+    gutterX: 40,
+    gutterY: 40,
+    baseWidth: 425
+});
